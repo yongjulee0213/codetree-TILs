04b3486c791f095bfb067ffd7efe13a0828960ca
Yongju Lee <72552929+yongjulee0213@users.noreply.github.com>
2024-07-07 14:03:10 +0900

    Title: 길이 단위 변환하기, Time: 162ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240707/길이 단위 변환하기/change-length-unit.js
@@ -0,0 +1,7 @@
+let ft = 9.2;
+let mi = 1.3;
+let ft2cm = 30.48;
+let mi2cm = 160934;
+
+console.log(ft + "ft" + " = " +(ft * ft2cm).toFixed(1) + "cm");
+console.log(mi + "mi" + " = " + (mi*mi2cm).toFixed(1) + "cm");
